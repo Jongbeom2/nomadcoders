778e108cb73d7bee05ac2a5e4423891205862638
jongbeom <n113345@gmail.com>
2020-03-10 21:37:42 +0900

    finish v js proj 1

--- Vanilla Javascript 1/public/src/todo.js
+++ Vanilla Javascript 1/public/src/todo.js
@@ -1,59 +1,67 @@
+const TODO_LS = 'toDos';
+
 const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
-const TODO_LS = 'toDos';
-
 let toDos = [];
 
-function deleteToDo(e){
+// delete todo from widget and local storage
+function deleteToDo(e) {
   const btn = e.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanTodos = toDos.filter(function(toDo){
+  const cleanTodos = toDos.filter(function (toDo) {
     return toDo.id !== parseInt(li.id);
   });
   toDos = cleanTodos;
   saveToDo();
 }
-function saveToDo(){
-  localStorage.setItem(TODO_LS,JSON.stringify(toDos));
+// save todo to local storage
+function saveToDo() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
 }
-function paintToDo(text){
-  const li = document.createElement("li");
-  const delBtn = document.createElement("button");
+// append todo to widget and local storage
+function paintToDo(text) {
   const span = document.createElement("span");
-  const newId = toDos.length+1;
+  const delBtn = document.createElement("button");
+  const li = document.createElement("li");
+  const newId = toDos.length + 1;
+  span.innerText = text + " ";
+  span.classList.add('toDoText');
   delBtn.innerText = '완료';
-  span.innerText = text+" ";
-  span.classList.add('toDo');
-  delBtn.addEventListener('click',deleteToDo);
+  delBtn.classList.add('toDoRemoveBtn')
+  delBtn.addEventListener('click', deleteToDo);
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = newId;
+  li.classList.add('toDo')
   toDoList.appendChild(li);
-  const toDoObj={
+  const toDoObj = {
     text,
-    id:newId
+    id: newId
   };
   toDos.push(toDoObj);
   saveToDo();
 }
-function handleSubmit(e){
+// submit todo
+function handleSubmit(e) {
   e.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
   toDoInput.value = '';
 }
-function loadToDos(){
+// load todo from local storage
+function loadToDos() {
   const loadedToDos = localStorage.getItem(TODO_LS);
-  if(loadedToDos !==null){
+  if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo){
+    parsedToDos.forEach(function (toDo) {
       paintToDo(toDo.text);
     });
   }
 }
+// init
 function init() {
   loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
